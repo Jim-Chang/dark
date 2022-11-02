6d1c1c9476dc0309f674dd9b1ffd15f07cb54fa9
Jim <jim@koding.work>
2022-11-02 20:35:15 +0800

    feat: implement content script

--- projects/chrome/src/content.ts
+++ projects/chrome/src/content.ts
@@ -1,1 +1,38 @@
-console.log('dark theme enable!');
+const configKeys = ['isEnable', 'excludeHosts'];
+const attrName = 'dark-theme-active';
+
+async function loadConfig() {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(configKeys, (items) => {
+      resolve(items);
+    });
+  });
+}
+
+async function render() {
+  const config = await loadConfig();
+  const isEnable = !!config['isEnable'];
+  const excludeHosts = config['excludeHosts'] ?? [];
+  const host = new URL(location.href).host;
+  if (isEnable && !excludeHosts.includes(host)) {
+    activeDark();
+  } else {
+    inactiveDark();
+  }
+}
+
+function activeDark() {
+  document.documentElement.setAttribute(attrName, '');
+}
+
+function inactiveDark() {
+  document.documentElement.removeAttribute(attrName);
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.configUpdate) {
+    render();
+  }
+});
+
+render();
