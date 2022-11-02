5ccfa463b0ec6b8df9d07029dddbc17450a54410
Jim <jim@koding.work>
2022-11-02 18:17:42 +0800

    feat: create popup-panel app

---
+++ projects/popup-panel/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
