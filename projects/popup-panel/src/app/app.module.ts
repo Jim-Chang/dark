2c373bfdac6c816a2374c5cb378b1b54a8dfa7aa
Jim <jim@koding.work>
2022-11-25 12:05:18 +0800

    feat: use material design to popup panel

--- projects/popup-panel/src/app/app.module.ts
+++ projects/popup-panel/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatSlideToggleModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
